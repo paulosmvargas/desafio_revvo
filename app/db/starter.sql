68707ea9f1b5f3968f18d47e9f539a71e6d5a8fa
paulosmvargas <paulosergiom29@gmail.com>
2025-02-26 11:41:50 -0300

    update sql

--- app/db/starter.sql
+++ app/db/starter.sql
@@ -1,16 +1,7 @@
-USE desafio_php;
-
 CREATE TABLE cursos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(255) NOT NULL,
     descricao TEXT NOT NULL,
-    imagem VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE slideshow (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(255) NOT NULL,
-    descricao TEXT NOT NULL,
     imagem VARCHAR(255) NOT NULL,
-    link VARCHAR(255) NOT NULL
+    slideshow VARCHAR(255) NOT NULL
 );
