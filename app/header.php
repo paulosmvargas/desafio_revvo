1b54576aebd370346866cc2f3d81759a3f1fb12c
paulosmvargas <paulosergiom29@gmail.com>
2025-02-26 21:58:53 -0300

    header

---
+++ app/header.php
@@ -0,0 +1,13 @@
+<header class="header">
+    <div class="logo">
+        <img src="public/images/logo.png" alt="Logo">
+    </div>
+    <div class="search-bar">
+        <input type="text" placeholder="Pesquisar...">
+        <button type="submit">🔍</button>
+    </div>
+    <div class="user-info">
+        <span>Jhon Doe</span>
+        <img src="public/images/user.png" alt="Usuário">
+    </div>
+</header>
